82eaa39fbd303ac1c69fd6b382ac01d3172a27ef
beniman8 <benik@live.ca>
2019-05-17 22:21:57 -0400

    files

---
+++ demo/settings/production.py
@@ -0,0 +1,39 @@
+from .base import *
+
+DEBUG = False
+
+ALLOWED_HOSTS = ['ip-adress','www.yourwebsite.com']
+
+
+# Password validation
+# https://docs.djangoproject.com/en/2.2/ref/settings/#auth-password-validators
+
+AUTH_PASSWORD_VALIDATORS = [
+    {
+        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    },
+    {
+        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    },
+]
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'your-db-name',
+        'USER': 'your-db-username',
+        'PASSWORD': ' your-db-password',
+        'HOST':'localhost',
+        'PORT':'',
+    }
+}
+
+
+STRIPE_PUBLIC_KEY = ''
+STRIPE_SECRET_KEY = ''
